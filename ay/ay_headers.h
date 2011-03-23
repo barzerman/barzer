b0d8136b5f536d8130ff1f6e46d292ebb09f3e5e
yanis <andrey.yanis@gmail.com>
2011-03-23 01:24:45 -0400

    new file

---
+++ ay/ay_headers.h
@@ -0,0 +1,16 @@
+#ifndef AY_HEADERS_H
+#define AY_HEADERS_H
+// standard headers 
+
+#include <algorithm>
+#include <cctype>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <cstdlib>
+#include <cstring>
+#include <memory>
+#include <sys/types.h>
+#include <typeinfo>
+
+#endif //AY_HEADERS_H
