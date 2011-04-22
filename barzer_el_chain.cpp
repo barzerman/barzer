d43294852ba29c0f7a6b3c6bb848ca9ec2e15f2f
barzerman <andrey.yanis@gmail.com>
2011-04-22 11:14:04 -0400

    minor cosmetics

--- barzer_el_chain.cpp
+++ barzer_el_chain.cpp
@@ -9,9 +9,6 @@ void BarzelBead::init( const CTWPVec::value_type& v )
 	ctokOrigVec.push_back( v );
 	dta = BarzelBeadAtomic();
 
-#warning "this had to fix this function. please check"
-
-	//BarzelBeadAtomic_var& atomic = boost::get< BarzelBeadAtomic >( dta ).dta;
 	BarzelBeadAtomic &a = boost::get< BarzelBeadAtomic >( dta );
 
 	const CToken& ct = v.first;
@@ -19,33 +16,26 @@ void BarzelBead::init( const CTWPVec::value_type& v )
 	case CTokenClassInfo::CLASS_UNCLASSIFIED: 
 		break;
 	case CTokenClassInfo::CLASS_BLANK: 
-		//boost::get<BarzerLiteral>(atomic).setBlank();
 		a.dta = BarzerLiteral();
 		boost::get<BarzerLiteral>(a.dta).setBlank();
 		break;
 	case CTokenClassInfo::CLASS_WORD: 
-		//boost::get<BarzerLiteral>(atomic).setString(ct.getStringId());
 		a.dta = BarzerLiteral();
 		boost::get<BarzerLiteral>(a.dta).setString(ct.getStringId());
 		break;
 	case CTokenClassInfo::CLASS_MYSTERY_WORD: 
-		// this was throwing boost::bad_get
-		//boost::get<BarzerString>(atomic).setFromTTokens( ct.getTTokens() );
 		a.dta = BarzerString();
 		boost::get<BarzerString>(a.dta).setFromTTokens( ct.getTTokens() );
 		break;
 	case CTokenClassInfo::CLASS_NUMBER:
-		//boost::get<BarzerNumber>(atomic) = ct.getNumber();
 		a.dta = BarzerNumber(ct.getNumber());
 		break;
 	case CTokenClassInfo::CLASS_PUNCTUATION:
-		//boost::get<BarzerLiteral>(atomic).setPunct(ct.getPunct());
 		a.dta = BarzerLiteral();
 		boost::get<BarzerLiteral>(a.dta).setPunct(ct.getPunct());
 		break;
 		
 	case CTokenClassInfo::CLASS_SPACE:
-		//boost::get<BarzerLiteral>(atomic).setBlank();
 		a.dta = BarzerLiteral();
 		boost::get<BarzerLiteral>(a.dta).setBlank();
 		break;
