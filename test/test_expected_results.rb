04210c6a2c7249b281ca4a54b3bc7a2156541d5e
Sergei Barbarash <sgt.sgt@gmail.com>
2011-05-17 17:11:35 +0400

    .

--- test/test_expected_results.rb
+++ test/test_expected_results.rb
@@ -10,7 +10,7 @@ class TestExpectedResults < Test::Unit::TestCase
       query = t["query"][0]
       expected_barz = t["barz"]
       current_barz = current_results["test"].find { |x| x["query"][0] == query }["barz"]
-      assert_equal current_barz, expected_barz, "checking barz for '#{query}'"
+      assert_equal expected_barz, current_barz, "checking barz for '#{query}'"
     end
   end
 end
