9f818636df3d25a030fb1abfeb8b6384afb539f0
Max Eronin <reductor@gmail.com>
2011-05-25 02:00:53 +0400

    .

--- barzer_universe.cpp
+++ barzer_universe.cpp
@@ -27,6 +27,7 @@ static GenericEntData g_genDta[] = {
 	GenericEntData(5,"area"), // square area
 	GenericEntData(6,"volume"), // cubic volume
 	GenericEntData(7,"wearsize"), // clothing/shoe sizes
+	GenericEntData(8,"time"), // time
 
 	// can't take this shit!
 	GenericEntData(69,"shit", "some shit"), // weight
