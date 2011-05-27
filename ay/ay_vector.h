639bea0097f54e82b7c4ad13c004938f8219336b
yanis <andrey.yanis@gmail.com>
2011-05-27 14:57:40 -0400

    vecmap added and tested

--- ay/ay_vector.h
+++ ay/ay_vector.h
@@ -3,6 +3,11 @@
 
 #include <ay_headers.h>
 #include <vector>
+
+
+// this file has various vector-based containers such as 
+// slogrovector, vecmap etc 
+
 // slogrovector is something that starts with a presumed size and then 
 // increments capacity by a fixed number of elements instead of doubling
 // this is needed for mostly static large vectors , whose approximate future size 
@@ -48,6 +53,91 @@ public:
 		vec.resize( vec.size() +1 ) ;
 		return vec.back();
 	}
+};
+
+/// vecmap keeps the vector of Key Value pairs with unique keys
+/// it mainly implements the basic iterations
+template <typename K, typename V, typename Comp = std::less<K> >
+class vecmap {
+public:
+	typedef std::pair< K,V> value_type;
+	typedef V data_type;
+	typedef K key_type;
+	typedef Comp key_comp;
+
+	struct value_comp {
+		bool operator() ( const value_type& l, const value_type& r ) const
+		{ return (key_comp()( l.first, r.first )); }
+
+		bool operator() ( const value_type& l, const key_type& r ) const
+		{ return (key_comp()( l.first, r )); }
+		bool operator() ( const key_type& l, const value_type& r ) const
+		{ return (key_comp()( l, r.first )); }
+		
+	};
+
+	typedef std::vector< value_type > TheVec;
+	
+	typedef typename TheVec::iterator iterator;
+	typedef typename TheVec::const_iterator const_iterator;
+private:
+	TheVec d_vec;
+public:
+	iterator begin() { return d_vec.begin(); }
+	const_iterator begin() const { return d_vec.begin(); }
+
+	iterator end() { return d_vec.end(); }
+	const_iterator end() const { return d_vec.end(); }
+	
+	iterator lower_bound( const K& k ) 
+		{ return std::lower_bound( d_vec.begin(), d_vec.end(), k, value_comp() ); }
+	const_iterator lower_bound( const K& k ) const
+		{ return std::lower_bound( d_vec.begin(), d_vec.end(), k, value_comp() ); }
+
+	iterator find( const K& k ) 
+	{ iterator i = lower_bound(k); return( (i == d_vec.end() || value_comp()(k,*i) ) ?  d_vec.end():i) ; }
+	const_iterator find( const K& k ) const
+	{ const_iterator i = lower_bound(k); return( (i == d_vec.end() || value_comp()(k,*i) ) ?  d_vec.end():i) ; }
+	
+	std::pair< iterator, iterator > equal_range( const key_type& k ) 
+		{ return std::equal_range( d_vec.begin(), d_vec.end(), k, value_comp() ); }
+	std::pair< const_iterator, const_iterator > equal_range( const key_type& k ) const
+		{ return std::equal_range( d_vec.begin(), d_vec.end(), k, value_comp() ); }
+
+	iterator upper_bound( const K& k ) 
+		{ return std::upper_bound( d_vec.begin(), d_vec.end(), k, value_comp() ); }
+	const_iterator upper_bound( const K& k ) const
+		{ return std::upper_bound( d_vec.begin(), d_vec.end(), k, value_comp() ); }
+
+	std::pair< iterator, bool> insert( const value_type& v )
+	{
+		iterator i = lower_bound( v.first );
+		if( i == d_vec.end() ||  value_comp()( v.first,*i) ) { // first non smaller is not equal or all keys smaller
+			return std::pair< iterator, bool>( d_vec.insert( i, v ), true );
+		} else {
+			return std::pair< iterator, bool>( i, false );
+		}
+	}
+	std::pair< iterator, bool> insert( const key_type& k, const data_type& d  )
+	{
+		iterator i = lower_bound( k );
+		if( i == d_vec.end() ||  value_comp()(k,*i) ) { // first non smaller is not equal or all keys smaller
+			return std::pair< iterator, bool>( d_vec.insert( i, value_type(k,d) ), true );
+		} else {
+			return std::pair< iterator, bool>( i, false );
+		}
+	}
+
+
+	void clear() { d_vec.clear(); }
+	iterator erase( iterator pos ) { return d_vec.erase(pos); }
+	iterator erase( iterator first, iterator last ) { return d_vec.erase(first,last); }
+
+	size_t size() const { return d_vec.size(); }
+
+	/// i has to be a number, not K
+	value_type& operator[] ( size_t i ) { return d_vec[i]; }
+	const value_type& operator[] ( size_t i ) const { return d_vec[i]; }
 };
 
 }
