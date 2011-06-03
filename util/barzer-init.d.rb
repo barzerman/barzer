2aebaf5fa84277a0951598e7d6191efce1513c42
Sergei Barbarash <sgt.sgt@gmail.com>
2011-06-03 16:28:16 +0400

    new init.d script ready for testing

--- util/barzer-init.d.rb
+++ util/barzer-init.d.rb
@@ -24,39 +24,53 @@ def usage
   puts "Usage: #{File.basename(__FILE__)} {start|stop|status|restart}"
 end
 
+def daemon_running?(daemon_name)
+  system("#{DAEMON_BIN} --name=#{daemon_name} --runninig")
+end
+
+def pid_file(i)
+  "#{PID_DIR}/barzer-#{i['name']}.pid"
+end
+
+def get_pid(i)
+  f = File.open(pid_file(i))
+  pid = f.readline.to_i
+  f.close
+  pid
+end
+
+def start_instance(i)
+  log_file = "#{LOG_DIR}/barzer-#{i['name']}.log"
+
+  # Making sure pid file is writable by barzer user
+  FileUtils.touch(pidfile) unless File.exist?(pidfile)
+  FileUtils.chown(BARZER_USER, pidfile)
+  
+  daemon_args = ["--name=barzer-#{i['name']}",
+                 "--inherit",
+                 "--chdir=#{BARZER_DIR}", 
+                 "--output=#{log_file}",
+                   "--pidfile=#{pid_file(i)}"].join(' ')
+  
+  if daemon_running?(i['name'])
+    puts "Instance #{i['name']} is already running."
+  else
+    barzer_cmd = "#{BARZER_BIN} server #{i['port']} -cfg #{CONFIG_DIR}/#{i['config']}.xml"
+    start_success = system("#{SU_BIN} -l #{BARZER_USER} --shell=/bin/bash -c \"#{DAEMON_BIN} #{daemon_args} -- #{barzer_cmd}\"")
+    if start_success
+      puts "Instance #{i['name']} started successfully."
+      return true
+    else
+      puts "Instance #{i['name']} failed to start."
+      return false
+    end
+  end
+end
+
 def start(instances)
   puts "Starting barzer instances..."
 
-  instances.each do |i|
-    
-    pidfile = "#{PID_DIR}/barzer-#{i['name']}.pid"
-    logfile = "#{LOG_DIR}/barzer-#{i['name']}.log"
-
-    # Making sure pid file is writable by barzer user
-    FileUtils.touch(pidfile) unless File.exist?(pidfile)
-    FileUtils.chown(BARZER_USER, pidfile)
-
-    daemon_args = ["--name=barzer-#{i['name']}",
-                   "--inherit",
-                   "--chdir=#{BARZER_DIR}", 
-                   "--output=#{logfile}",
-                   "--pidfile=#{pidfile}"].join(' ')
-
-    is_running = system("#{DAEMON_BIN} #{daemon_args} --runninig")
-
-    if is_running
-      puts "Instance #{i['name']} is already running."
-    else
-      barzer_cmd = "#{BARZER_BIN} server #{i['port']} -cfg #{CONFIG_DIR}/#{i['config'].xml"
-      start_success = system("#{SU_BIN} -l #{BARZER_USER} --shell=/bin/bash -c \"#{DAEMON_BIN} #{daemon_args} -- #{barzer_cmd}\"")
-      if start_success
-        puts "Instance #{i['name']} started successfully."
-      else
-        puts "Instance #{i['name']} failed to start."
-      end
-    end
-  end
-  
+  instances.each { |i| start_instance(i) }
 end
 
 def processes_running?
@@ -67,15 +81,46 @@ def force_stop
   system("killall -u #{BARZER_USER} #{BARZER_BIN} daemon") if processes_running?
 end
 
+def stop_instance(i)
+  if daemon_running?(i['name'])
+    system("${DAEMON_BIN --name=#{i['name']} --stop")
+
+    tries = 0
+    while daemon_running?(i['name']) && tries < 5 do
+      tries += 1
+    end
+
+    return tries < 5
+  else
+    return true
+  end
+end
+
 def stop(instances)
   puts "Stopping barzer instances..."
+
+  stop_failures = []
+  instances.each { |i| stop_failures << i['name'] unless stop_instance(i) }
+  force_stop if stop_failures
+
+  # Many daemons don't delete their pidfiles when they exit.
+  instances.each { |i| File.delete(pid_file(i)) }
 end
 
 def status(instances)
+  instances.each do |i|
+    if daemon_running(i)
+      puts "Instance #{i['name']} is running with the pid #{get_pid(i)}"
+    else
+      # TODO: detect stray processes and stray pid files
+    end
+  end
 end
 
 def restart(instances)
   puts "Restarting barzer instances..."
+  stop(instances)
+  start(instances)
 end
 
 def main
