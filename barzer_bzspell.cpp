c244c22eaf9ab16306789170525670cd495f8569
yanis <andrey.yanis@gmail.com>
2011-08-25 11:25:08 -0400

    .

---
+++ barzer_bzspell.cpp
@@ -0,0 +1,93 @@
+#include <barzer_universe.h>
+#include <barzer_bzspell.h>
+
+namespace barzer {
+
+void BZSpell::addExtraWordToDictionary( uint32_t strId, uint32_t frequency )
+{
+	BZSWordInfo& wi = d_wordinfoMap[ strId ];
+	if( wi.upgradePriority( BZSWordInfo::WPRI_USER_EXTRA ) )
+		wi.setFrequency( frequency );
+}
+
+/// when fails 0xffffffff is returned 
+uint32_t BZSpell::getSpellCorrection( const char* s ) const
+{
+	return 0xffffffff;
+}
+uint32_t BZSpell::getStem( const char* s ) const
+{
+	return 0xffffffff;
+}
+
+namespace {
+
+struct WPCallback {
+	BZSpell& bzs;
+	uint32_t fullStrId;
+	size_t   varCount; 
+	WPCallback( BZSpell& b, uint32_t l2  ) : bzs(b), fullStrId(l2), varCount(0)  {}
+	typedef std::vector<char> charvec;
+	typedef charvec_ci::const_iterator charvec_ci;
+
+	int operator()( charvec_ci fromI, charvec_ci toI )
+	{
+		charvec v( fromI, toI );
+		charvec.push_back(0);
+		GlobalPools& gp = bzs.getUniverse().getGlobalPools();
+		const char* str = &(v[0]);
+
+		uint32_t strId = gp.string_intern( str );
+
+		bzs.addWordToLinkedWordsMap( strId, fullStrId );
+		++varCount;
+		return 0;
+	}
+};
+
+}
+
+size_t BZSpell::produceWordVariants( uint32_t strId )
+{
+	/// for ascii 
+	const char* str = d_universe.getGlobalPools().string_resolve( strId );
+	if( !str ) 
+		return 0;
+
+	WPCallback cb( *this, strId );	
+	ay::choose_n<char, WPCallback > variator( cb, strId );
+	variator();
+	return cb.varCount;
+}
+
+size_t BZSpell::init( const StoredUniverse* secondaryUniverse ) const
+{
+	if( secondaryUniverse ) {
+		d_secondarySpellchecker = secondaryUniverse->getBZSpell();
+	} else {
+		const UniverseTrieCluster::BELTrieList& trieList = d_universe.getTrieList(); 
+		for( UniverseTrieCluster::BELTrieList::const_iterator t = trieList.begin(); t!= trieList.end(); ++t ) {
+			const strid_to_triewordinfo_map& wiMap = t->getWordInfoMap();
+			for( strid_to_triewordinfo_map::const_iterator w = wiMap.begin(); w != wiMap.end(); ++w ) {
+				const TrieWordInfo& wordInfo = w->second;
+				uint32_t strId = w->first;
+	
+				BZSWordInfo& wi = d_wordinfoMap[ strId ];
+				
+				if( wi.upgradePriority( trie->getSpellPriority()) )
+					wi.setFrequency( wi.wordCount );
+			}
+		}
+	}
+
+	/// at this point d_wordinfoMap has an entry for every word 
+	/// now we will generate edit distance 1 variants for each word 
+	for( strid_wordinfo_hmap::const_iterator wi = d_wordinfoMap.begin(); wi!= d_wordinfoMap.end(); ++wi ) {
+		uint32_t strId = wi->first;
+
+		produceWordVariants( strId );
+	}
+	return d_linkedWordsMap.size();
+}
+
+} // namespace barzer
---
+++ barzer_bzspell.h
@@ -0,0 +1,101 @@
+#ifndef BARZER_BZSPELL_H 
+#define BARZER_BZSPELL_H 
+
+#include <ay_evovec.h>
+#include <boost/unordered_map.hpp>
+
+namespace barzer {
+
+/// for every word visible by the user this is stored int he universe
+struct BZSWordInfo {
+	// 0 - generic fluff 
+	// 1 - user specific extra words 
+	// 5 - generic non fluff
+	// 10 - user specific fluf
+	// 15 - user specific non fluff
+	// ... the higher the number the "heavier" the word is and the likely it is to be selected 
+	// by the spellchecker 
+	uint8_t d_priority;
+
+	/// frequency of the given word among the user-visible words
+	// 1-100 - the higher the score the heavier the word - this is the 2-nd order factor
+	/// initially this may contain occurence counts
+	uint32_t d_freq;
+	enum {
+		WPRI_GENERIC_FLUFF = 0,
+		WPRI_USER_EXTRA = 1,
+		WPRI_GENERIC_NONFLUFF = 5,
+		WPRI_USER_FLUFF = 10,
+		WPRI_USER_NONFLUFF = 15
+	};
+
+	void setFrequency( uint32_t f ) { d_freq= f; }
+	bool upgradePriority( uint8_t p ) 
+	{
+		if( p> d_priority ) {
+			d_priority= p;
+			return true;
+		} else 
+			return false;
+	}
+	uint32_t getPriority() const { return d_priority; }
+	uint32_t getFrequency() const { return d_freq; }
+};
+
+/// one of thes eis stored for each unique strId in a trie 
+struct BZSWordTrieInfo {
+	uint32_t wordCount; 
+	
+	BZSWordTrieInfo() : wordCount(0) {}
+	void incrementCount() { ++wordCount; }
+};
+typedef boost::unordered_map< uint32_t, BZSWordInfo > 
+
+class BZSpell {
+	StoredUniverse& d_universe;
+public:
+	/// the evos must be ordered by priority + frequency in descending order 
+	typedef boost::unordered_map< uint32_t, ay::evovec_uint32 > strid_evovec_hmap;
+	typedef boost::unordered_map< uint32_t, BZSWordInfo > strid_wordinfo_hmap;
+
+	/// the next spell checker in line  
+	const BZSpell* d_secondarySpellchecker; 
+private: 
+	strid_wordinfo_hmap d_wordinfoMap;	// from actual words to universe specific word info 
+	strid_evovec_hmap  	d_linkedWordsMap;  // words linked to partial word
+	
+	std::string d_extraWordsFileName;
+
+	/// generates edit distance variants 
+	size_t produceWordVariants( uint32_t strId ); 
+public:
+	void addWordToLinkedWordsMap(uint32_t linkTo, uint32_t strId )
+	{
+		d_linkedWordsMap[ linkTo ].push_back( strId );
+	}
+	StoredUniverse& getUniverse() { return d_universe; }
+	const StoredUniverse& getUniverse() const { return d_universe; }
+	void clear()
+	{
+		d_wordinfoMap.clear();
+		d_linkedWordsMap.clear();
+	}
+	void addExtraWordToDictionary( uint32_t, uint32_t frequency = 0 );
+
+	BZSpell( StoredUniverse& uni ) : d_universe( uni ) {}
+
+	// returns the size of strid_evovec_hmap
+	// this function should be called after the load. 
+	// specifically: after the last addExtraWordToDictionary has been called
+	size_t init( const StoredUniverse* secondaryUniverse =0 );
+
+	/// when fails 0xffffffff is returned 
+	uint32_t getSpellCorrection( const char* s ) const;
+	uint32_t getStem( const char* s ) const;
+
+	void loadExtra( const char* fileName );
+};
+
+}
+
+#define // BARZER_BZSPELL_H 
