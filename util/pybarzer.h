d7ff96f8f1d59d04753443924b2a51326ce5ac69
Max Eronin <reductor@gmail.com>
2012-02-04 08:09:39 +0400

    .

---
+++ util/pybarzer.h
@@ -0,0 +1,12 @@
+/*
+ * pybarzer.h
+ *
+ *  Created on: Feb 4, 2012
+ *      Author: polter
+ */
+
+#ifndef PYBARZER_H_
+#define PYBARZER_H_
+
+const std::string stripDiacritics(const std::string &);
+#endif /* PYBARZER_H_ */
