5e6787b3f072f720b2e860717d7007729888b5c1
nix <nikitos3000@yandex.ru>
2012-02-21 23:34:02 +0400

    counter improved

--- barzer_emitter.cpp
+++ barzer_emitter.cpp
@@ -345,18 +345,29 @@ void BELReaderXMLEmitCounter::addStatement(const barzer::BELStatementParsed& sp)
 namespace {
 struct SumProductFunctor
 {
-  bool isSum;
-  size_t& x;
-  const BELReaderXMLEmitCounter& c;
-  SumProductFunctor(const BELReaderXMLEmitCounter& counter, size_t& xx, bool i=true  ) 
-  :isSum(i),x(xx), c(counter)  
-  {x = isSum? 0:1;}
+    size_t type;
+    size_t& x;
+    const BELReaderXMLEmitCounter& c;
+    SumProductFunctor(const BELReaderXMLEmitCounter& counter, size_t& xx, size_t node_t  ) 
+    :type(node_t),x(xx), c(counter)  
+    {
+        if (type == BTND_StructData::T_ANY || type == BTND_StructData::T_TAIL) x = 0;
+        else x = 1;
+    }
   size_t operator() (const barzer::BELParseTreeNode& node ) const
   {
     if (x > BELReaderXMLEmitCounter::MAX_CONSIDERED) return x=BELReaderXMLEmitCounter::INF;
     size_t p = c.power(node);
     if (p > BELReaderXMLEmitCounter::MAX_CONSIDERED) return x=BELReaderXMLEmitCounter::INF;
-    else return x=(isSum? std::plus<size_t>()(x,p) : (p==0? std::multiplies<size_t>()(x,1):std::multiplies<size_t>()(x,p)));
+    else 
+    {   //can be optimised
+        switch (type) {
+            case BTND_StructData::T_ANY: return x = std::plus<size_t>()(x,p);   break;
+            case BTND_StructData::T_TAIL: return x==0? x = p : x = p*(1+x);     break;
+            case BTND_StructData::T_SUBSET: return x = x*(1 + p);               break;
+            default: return p==0? x=std::multiplies<size_t>()(x,1) : x=std::multiplies<size_t>()(x,p);
+        }       
+    }
   }
 };
 
@@ -369,40 +380,40 @@ size_t factorial( size_t n )
 }
 
 } // anon namespace 
+
 /// return BELReaderXMLEmitCounter::INF if the result exceedes BELReaderXMLEmitCounter::MAX_CONSIDERED
 size_t BELReaderXMLEmitCounter::power(const barzer::BELParseTreeNode& node) const
 {
-
-
-    switch (node.btndVar.which()) {
-    case BTND_StructData_TYPE: {
-      if (node.child.empty()) return 0;
-      const BTND_StructData &sdata = boost::get<BTND_StructData>(node.btndVar);
-      size_t p = 0;
-      size_t x = 0;
-      SumProductFunctor aggr(*this,p,(sdata.getType() == BTND_StructData::T_ANY) );
-      std::for_each( node.child.begin(), node.child.end(), aggr);
-      
-        switch (sdata.getType()) {
-        case BTND_StructData::T_LIST:   x = 1;                              break;
-        case BTND_StructData::T_ANY:    x = 1;                              break;
-        case BTND_StructData::T_OPT:    x = 2;                              break;  
-        case BTND_StructData::T_PERM:   x = factorial(node.child.size());   break;
-        case BTND_StructData::T_TAIL:   x = node.child.size();              break;
-        case BTND_StructData::T_SUBSET: x = exp2(node.child.size());        break;
+switch (node.btndVar.which()) {
+case BTND_StructData_TYPE: {
+    if (node.child.empty()) return 0;
+    const BTND_StructData &sdata = boost::get<BTND_StructData>(node.btndVar);
+    size_t p = 0;
+    SumProductFunctor aggr(*this, p, sdata.getType());
+    if (BTND_StructData::T_TAIL == sdata.getType()) 
+        for ( int i = node.child.size() - 1; i >= 0; i-- ) aggr(node.child[i]); //reverse order important!
+    else   std::for_each( node.child.begin(), node.child.end(), aggr);
+    
+    if (p > MAX_CONSIDERED) return INF;
+    switch (sdata.getType()) {
+        case BTND_StructData::T_LIST:   /*nothing*/;                            break;
+        case BTND_StructData::T_ANY:    /*nothing*/;                            break;
+        case BTND_StructData::T_OPT:    p++;                                    break;  
+        case BTND_StructData::T_PERM:   p *= factorial(node.child.size());      break;
+        case BTND_StructData::T_TAIL:   /*nothing*/;                            break;
+        case BTND_StructData::T_SUBSET: p--;                                    break;
         default:
             AYLOG(ERROR) << "Invalid BTND_StructData type: " << sdata.getType();
             return 0;
-        }
-        //debug: std::cout << "node="<< sdata.getType() <<" p=" <<p<<" x="<<x<< std::endl;
-        size_t answer = p*x;
-        return (p>=MAX_CONSIDERED || x >=MAX_CONSIDERED || answer >=MAX_CONSIDERED)?  INF : answer;
+    }
+        std::cout << "node="<< sdata.getType() <<" p=" <<p << std::endl;
+        return (p > MAX_CONSIDERED)? INF : p;
     }
     case BTND_PatternData_TYPE:
         return 1;
-    }
-    AYLOG(ERROR) << "Smth definitly broken.Invalid BTND_TYPE type: " << node.btndVar.which();
-    return 0;
+}
+AYLOG(ERROR) << "Smth definitly broken.Invalid BTND_TYPE type: " << node.btndVar.which();
+return 0;
 }
 
 }
