f060521aec878d26c921047fb5db7004bd55c725
Andre Yanpolsky <andrey.yanis@gmail.com>
2012-02-22 18:37:55 -0500

    .

--- barzer_emitter.cpp
+++ barzer_emitter.cpp
@@ -372,7 +372,7 @@ struct SumProductFunctor
 };
 
 
-size_t factorial( size_t n )
+inline size_t factorial( size_t n )
 {
   const size_t answer[] = {1,1,2,6,24,120,720,5040, 40320, 362880, 3628800, 39916800, 479001600};
   if (n > 12) return BELReaderXMLEmitCounter::INF;
