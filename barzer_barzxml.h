63e52e9f95fe9e1354dba75d7cdc22bee8f8a31d
andre yanpolsky <andrey.yanis@gmail.com>
2012-05-12 17:13:30 -0400

    .

--- barzer_barzxml.h
+++ barzer_barzxml.h
@@ -1,10 +1,12 @@
 #ifndef BARZER_BARZXML_H
 #define BARZER_BARZXML_H
+
 namespace barzer {
 
 class Barz;
 struct BarzXMLParser {
     Barz& barz;
+
     const StoredUniverse& universe;
 
     void takeTag( const char* tag, const char** attr, bool open=true );
