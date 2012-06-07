2713414049d7c3113057a9fdd20c1c92a50b8521
0xd34df00d <0xd34df00d@gmail.com>
2012-06-08 02:25:12 +0400

    Added CharUTF8::{to,set}UTF32().

--- ay/main.cpp
+++ ay/main.cpp
@@ -1,33 +1,24 @@
 #include <iostream>
 #include <iterator>
 #include <algorithm>
+#include <assert.h>
 #include "ay_utf8.h"
 
 using namespace ay;
 
-void print (CharUTF8 t)
-{
-	std::cout << t.getBuf ();
-}
-
 int main ()
 {
 	StrUTF8 lat ("хe");
-	lat.swap (lat.begin () + 1, lat.begin ());
 	StrUTF8 str ("$йу́хgbзд́а́€");
 	StrUTF8 str2 ("йу́х");
 	StrUTF8 str3 ("g€b");
 
-	std::cout << lat.c_str () << std::endl;
-	//std::for_each (str2.rbegin (), str2.rend (), print);
-
-	/*
-	str.print (std::cout);
-	std::cout << std::endl;
-
-	str.erase (str.begin () + 1, str.begin () + 4);
-	str.print (std::cout);
-	std::cout << std::endl;
-	*/
+	CharUTF8 b1("$");
+	CharUTF8 b2("¢");
+	CharUTF8 b3("€");
+	auto c = CharUTF8::fromUTF32(162);
+	assert (b1 == CharUTF8::fromUTF32(b1.toUTF32()));
+	assert (b2 == CharUTF8::fromUTF32(b2.toUTF32()));
+	assert (b3 == CharUTF8::fromUTF32(b3.toUTF32()));
 }
 
