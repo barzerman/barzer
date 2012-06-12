c188198092212b84ec3470491940aab78577b179
Andre Yanpolsky <andrey.yanis@gmail.com>
2012-06-11 20:17:03 -0400

    .

--- barzer_settings.cpp
+++ barzer_settings.cpp
@@ -182,7 +182,8 @@ void BarzerSettings::loadInstanceSettings() {
 			if( !nt )
 				nt = 1;
 			if( nt > MAX_REASONABLE_THREADCOUNT ) {
-				AYLOG(ERROR) << "threadcoutn of " << nt << " exceeds " <<  MAX_REASONABLE_THREADCOUNT << std::endl;
+				// AYLOG(ERROR) << "threadcoutn of " << nt << " exceeds " <<  MAX_REASONABLE_THREADCOUNT << std::endl;
+				std::cerr << "threadcoutn of " << nt << " exceeds " <<  MAX_REASONABLE_THREADCOUNT << std::endl;
 			}
 			setNumThreads( nt );
 		}
