32a6613961f65df9025ee9311e1eb74508cf3904
Max Eronin <reductor@gmail.com>
2012-07-09 21:54:33 +0400

    .

--- getdeplibs.py
+++ getdeplibs.py
@@ -4,22 +4,35 @@
 import sys, os, re
 
 
+uname = os.uname()[0]
 
 try:
     LDD, PAT = {
         'Linux': ('ldd', '=>\s+(.*)\s+\(0x[\da-f]+\)$'),
         'Darwin': ('otool -L', '^([^\(]+)\s+\(')
-    }[os.uname()[0]]
+    }[uname]
 except KeyError:
-    sys.stderr.write("Unknown OS\n")
+    sys.stderr.write("Unknown platform\n")
     sys.exit(1)
 
 RE = re.compile(PAT)
 
+STOPLIST = {
+    '*': {},
+    'Linux': {'libpython'},
+    'Darwin': {'libSystem'}
+}
+
+def in_stoplist(w):
+    n = os.path.basename(w).split('.', 1)[0]
+    return (n in STOPLIST['*'] or n in STOPLIST[uname])
+
+
+
 g_visited=set()
 def recurse(arr):
     for i in arr:
-        if len(i)>0 and i not in g_visited:
+        if len(i) > 0 and not (in_stoplist(i) or i in g_visited):
             g_visited.add(i)
             descendArr=[]
             for line in os.popen("{0} {1}".format(LDD, i)):
@@ -30,11 +43,12 @@ def recurse(arr):
                         descendArr.append(fname)
             recurse(descendArr)
 
-if len(sys.argv)<=1:
+
+if len(sys.argv) <= 1:
     sys.stderr.write( "usage: getdeplibs.py <file>\n")
     sys.exit(2)
 firstbin= sys.argv[1] if len(sys.argv)>1 else "./barzer.exe"
-arr=[firstbin]
+arr = [firstbin]
 recurse(arr)
 
 for i in g_visited:
