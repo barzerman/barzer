37187dcf9f6d9350176481c713e62e59b5344069
0xd34df00d <0xd34df00d@gmail.com>
2012-08-24 17:42:45 +0400

    Changed formatting in heuristics.

--- barzer_spellheuristics.cpp
+++ barzer_spellheuristics.cpp
@@ -4,81 +4,81 @@
 
 namespace barzer
 {
-	void SoundsLikeInfo::addSource(uint32_t soundsLike, uint32_t source)
-	{
-		SourceDictionary_t::iterator pos = m_sources.find(soundsLike);
-		if (pos == m_sources.end()) 
-			pos = m_sources.insert(std::make_pair(soundsLike, ay::StackVec<uint32_t>())).first;
+void SoundsLikeInfo::addSource(uint32_t soundsLike, uint32_t source)
+{
+	SourceDictionary_t::iterator pos = m_sources.find(soundsLike);
+	if (pos == m_sources.end()) 
+		pos = m_sources.insert(std::make_pair(soundsLike, ay::StackVec<uint32_t>())).first;
 
-        pos->second.push_back(source);
-	}
+	pos->second.push_back(source);
+}
 
-	const SoundsLikeInfo::SourceList_t* SoundsLikeInfo::findSources(uint32_t like) const
-	{
-		SourceDictionary_t::const_iterator pos = m_sources.find(like);
-		return pos == m_sources.end() ? 0 : &pos->second;
-	}
+const SoundsLikeInfo::SourceList_t* SoundsLikeInfo::findSources(uint32_t like) const
+{
+	SourceDictionary_t::const_iterator pos = m_sources.find(like);
+	return pos == m_sources.end() ? 0 : &pos->second;
+}
+
+void HashingSpellHeuristic::addSource(const char *sourceWord, size_t len, uint32_t sourceId)
+{
+	std::string out;
+	transform(sourceWord, len, out);
 	
-	void HashingSpellHeuristic::addSource(const char *sourceWord, size_t len, uint32_t sourceId)
+	const uint32_t soundsId = m_gp.internString_internal(out.c_str(), out.size());
+	m_mapping.addSource(soundsId, sourceId);
+}
+
+const SoundsLikeInfo::SourceList_t* HashingSpellHeuristic::findSources(const char *domainWord, size_t len) const
+{
+	std::string out;
+	transform(domainWord, len, out);
+	
+	const uint32_t id = m_gp.internalString_getId(out.c_str());
+	return m_mapping.findSources(id);
+}
+
+void EnglishSLHeuristic::transform(const char *src, size_t srcLen, std::string& out) const
+{
+	ay::tl::en2ru(src, srcLen, out);
+}
+
+void RuBastardizeHeuristic::transform(const char *src, size_t srcLen, std::string& out) const
+{
+	char prev1 = 0;
+	char prev2 = 0;
+	for (const char *pos = src, *end = src + srcLen; pos < end; pos += 2)
 	{
-		std::string out;
-		transform(sourceWord, len, out);
+		if (ay::tl::is_not_russian_consonant(pos) || (pos[0] == prev1&& pos[1] == prev2) )
+			continue;
 		
-		const uint32_t soundsId = m_gp.internString_internal(out.c_str(), out.size());
-		m_mapping.addSource(soundsId, sourceId);
+		out.append(pos, 2);
+		prev1 = pos[0];
+		prev2 = pos[1];
 	}
-	
-	const SoundsLikeInfo::SourceList_t* HashingSpellHeuristic::findSources(const char *domainWord, size_t len) const
+}
+
+ChainHeuristic::ChainHeuristic(const HashingSpellHeuristic& in, const HashingSpellHeuristic& out)
+: HashingSpellHeuristic(in.getGP())
+, m_in(in)
+, m_out(out)
+{
+}
+
+void ChainHeuristic::transform(const char *src, size_t srcLen, std::string& out) const
+{
+	std::string temp;
+	m_in.transform(src, srcLen, temp);
+	m_out.transform(temp.c_str(), temp.size(), out);
+}
+
+void ChainListHeuristic::transform(const char *src, size_t srcLen, std::string& out) const
+{
+	std::string prevResult(src, srcLen);
+	for (size_t i = 0; i < m_heuristics.size(); ++i)
 	{
-		std::string out;
-		transform(domainWord, len, out);
-		
-		const uint32_t id = m_gp.internalString_getId(out.c_str());
-		return m_mapping.findSources(id);
+		out.clear();
+		m_heuristics[i]->transform(prevResult.c_str(), prevResult.size(), out);
+		prevResult = out;
 	}
-	
-	void EnglishSLHeuristic::transform(const char *src, size_t srcLen, std::string& out) const
-	{
-		ay::tl::en2ru(src, srcLen, out);
-	}
-	
-	void RuBastardizeHeuristic::transform(const char *src, size_t srcLen, std::string& out) const
-	{
-		char prev1 = 0;
-		char prev2 = 0;
-		for (const char *pos = src, *end = src + srcLen; pos < end; pos += 2)
-		{
-			if (ay::tl::is_not_russian_consonant(pos) || (pos[0] == prev1&& pos[1] == prev2) )
-				continue;
-			
-			out.append(pos, 2);
-			prev1 = pos[0];
-			prev2 = pos[1];
-		}
-	}
-	
-	ChainHeuristic::ChainHeuristic(const HashingSpellHeuristic& in, const HashingSpellHeuristic& out)
-	: HashingSpellHeuristic(in.getGP())
-	, m_in(in)
-	, m_out(out)
-	{
-	}
-	
-	void ChainHeuristic::transform(const char *src, size_t srcLen, std::string& out) const
-	{
-		std::string temp;
-		m_in.transform(src, srcLen, temp);
-		m_out.transform(temp.c_str(), temp.size(), out);
-	}
-	
-	void ChainListHeuristic::transform(const char *src, size_t srcLen, std::string& out) const
-	{
-		std::string prevResult(src, srcLen);
-		for (size_t i = 0; i < m_heuristics.size(); ++i)
-		{
-			out.clear();
-			m_heuristics[i]->transform(prevResult.c_str(), prevResult.size(), out);
-			prevResult = out;
-		}
-	}
+}
 }
