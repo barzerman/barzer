8cb75336a677b95bebbeba9fde44ac7e60c39412
barzerman <andrey.yanis@gmail.com>
2012-09-27 21:07:30 -0400

    shit

--- barzer.mk
+++ barzer.mk
@@ -20,11 +20,12 @@ endif
 #    C11=-std=c++0x
 #endif
 C11=-std=c++0x
+C11LIB=-stdlib=libc++
 ifeq ($(CC),clang++)
     CLANG_WARNSUPPRESS=-Wno-array-bounds
 endif
 WARNSUPPRESS=-Wno-parentheses -Wnon-virtual-dtor $(CLANG_WARNSUPPRESS)
-CFLAGS :=$(CFLAGS) $(BITMODE) $(OPT) $(WARNSUPPRESS) $(C11)\
+CFLAGS :=$(CFLAGS) $(BITMODE) $(OPT) $(WARNSUPPRESS) $(C11) $(C11LIB)\
 	-I/opt/local/include -I/usr/include -g -I. -I./ay -I./lg_ru -fpic $(PYINCLUDE)
 LINKFLAGS := $(FLAGS)
 BINARY=barzer.exe
