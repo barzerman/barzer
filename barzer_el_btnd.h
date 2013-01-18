6641cecce61f5499158bb0011e5c122a740b3286
barzerman <andrey.yanis@gmail.com>
2013-01-18 16:12:23 -0500

    #441

--- barzer_el_btnd.h
+++ barzer_el_btnd.h
@@ -49,7 +49,7 @@ struct BTND_Pattern_Base {
 /// simple number wildcard data 
 struct BTND_Pattern_Number : public BTND_Pattern_Base {
 	std::ostream& print( std::ostream&, const BELPrintContext& ) const;
-	enum {
+	enum : uint8_t {
 		T_ANY_INT, // any integer 
 		T_ANY_REAL, // any real
 
@@ -60,13 +60,21 @@ struct BTND_Pattern_Number : public BTND_Pattern_Base {
 		T_MAX
 	};
 
-	uint8_t d_asciiLen;
-	uint8_t type; // one of T_XXXX 
+	enum : uint8_t {
+        RT_FULL,
+        RT_NOHI,
+        RT_NOLO
+    };
+
 	// for EXACT_XXX types only lo is used
 	union {
 		struct { float lo,hi; } real;
 		struct { int lo,hi; } integer;
 	} range;
+
+	uint8_t d_asciiLen;
+	uint8_t type; // one of T_XXXX 
+	uint8_t rangeType; // RT_XXX default RT_FULL
 	
 	uint8_t getAsciiLen() const { return d_asciiLen; }
 	void 	setAsciiLen( uint8_t i ) { d_asciiLen= i; }
@@ -108,22 +116,51 @@ struct BTND_Pattern_Number : public BTND_Pattern_Base {
 
 	bool isReal() const { return (type == T_ANY_REAL || type == T_RANGE_REAL); }
 
-	BTND_Pattern_Number() : d_asciiLen(0), type(T_ANY_INT) { range.integer.lo = range.integer.hi = 0; }
+	BTND_Pattern_Number() : 
+        d_asciiLen(0), 
+        type(T_ANY_INT) ,
+        rangeType(RT_FULL)
+    { range.integer.lo = range.integer.hi = 0; }
 
 	void setAnyNumber() { type = T_ANY_NUMBER; }
 	void setAnyInt() { type = T_ANY_INT; }
 	void setAnyReal() { type = T_ANY_REAL; }
 
 	int getType() const { return type; }
+	void setIntRange_Partial( int x, bool noHi ) 
+    {
+		type = T_RANGE_INT;
+        if( noHi ) {
+		    range.integer.lo = x;
+		    rangeType = RT_NOHI;
+        } else {
+		    range.integer.hi = x;
+		    rangeType = RT_NOLO;
+        }
+    }
 	void setIntRange( int lo, int hi )
 	{
 		type = T_RANGE_INT;
+        rangeType = RT_FULL;
+
 		range.integer.lo = lo;
 		range.integer.hi = hi;
 	}
+	void setRealRange_Partial( float x, bool noHi )
+    {
+		type = T_RANGE_REAL;
+        if( noHi ) {
+		    range.real.lo = x;
+		    rangeType = RT_NOHI;
+        } else { 
+		    range.real.hi = x;
+		    rangeType = RT_NOLO;
+        }
+    }
 	void setRealRange( float lo, float hi )
 	{
 		type = T_RANGE_REAL;
+        rangeType = RT_FULL;
 		range.real.lo = lo;
 		range.real.hi = hi;
 	}
