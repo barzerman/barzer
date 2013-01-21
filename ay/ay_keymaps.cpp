d3acad7ad2c5d5d1fe9fffe3d2a9490d9fb618e4
0xd34df00d <0xd34df00d@gmail.com>
2013-01-21 18:21:54 +0400

    Slightly optimized ay::km::engToRus (direct table indexing).

--- ay/ay_keymaps.cpp
+++ ay/ay_keymaps.cpp
@@ -1,12 +1,38 @@
 #include "ay_keymaps.h"
+#include <iostream>
 #include "ay_utf8.h"
 
 namespace ay
 {
 namespace km
 {
-	static const char qwerty[] = "qwertyuiop[]asdfghjkl;'zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>";
-	static const StrUTF8 ycuken ("йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ");
+	namespace
+	{
+		const char qwerty[] = "qwertyuiop[]asdfghjkl;'zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>";
+		const StrUTF8 ycuken ("йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ");
+
+		const struct TableGenerator
+		{
+			enum { TableSize = 127 };
+
+			typedef char RuLetter_t[2];
+			RuLetter_t m_table[TableSize];
+
+			TableGenerator ()
+			: m_table{}
+			{
+				for (unsigned char i = 0; i < TableSize; ++i)
+				{
+					const char *pos = strchr(qwerty, i);
+
+					if (pos)
+						ycuken[pos - qwerty].copyToBufNoNull(m_table[i]);
+					else
+						m_table[i][0] = m_table[i][1] = 0;
+				}
+			}
+		} TableGen;
+	}
 
 	bool engToRus(const char *eng, size_t size, std::string& rus)
 	{
@@ -15,12 +41,11 @@ namespace km
 		for (size_t i = 0; i < size; ++i)
 		{
 			const char ch = eng[i];
-			const char *pos = strchr(qwerty, ch);
-			if (!pos)
+			const auto letter = TableGen.m_table[static_cast<size_t>(ch < 127 && ch > 0 ? ch : 0)];
+			if (!*reinterpret_cast<const uint16_t*>(letter))
 				continue;
 
-			rus.append(ycuken[pos - qwerty], 2);
-
+			rus.append(letter, 2);
 			converted = true;
 		}
 
