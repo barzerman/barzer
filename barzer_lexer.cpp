7641bdf4528ec9016130c8b5c38d9a7afe338569
0xd34df00d <0xd34df00d@gmail.com>
2013-01-22 08:20:18 +0400

    Smarter dontspell.

--- barzer_lexer.cpp
+++ barzer_lexer.cpp
@@ -767,9 +767,6 @@ inline bool QLexParser::trySplitCorrect ( SpellCorrectResult& corrResult, QLexPa
 
 SpellCorrectResult QLexParser::trySpellCorrectAndClassify (PosedVec<CTWPVec> cPosVec, PosedVec<TTWPVec> tPosVec, const QuestionParm& qparm)
 {
-	if (m_dontSpell)
-		return SpellCorrectResult (0, ++cPosVec, ++tPosVec);
-	
 	CToken& ctok = cPosVec.element().first;
 	TToken& ttok = tPosVec.element().first;
 
@@ -913,7 +910,7 @@ SpellCorrectResult QLexParser::trySpellCorrectAndClassify (PosedVec<CTWPVec> cPo
 	}
 
 	if( !isUsersWord ) {
-		strId = bzSpell->getSpellCorrection( theString, true, lang );
+		strId = m_dontSpell ? 0xffffffff : bzSpell->getSpellCorrection( theString, true, lang );
 		if( strId != 0xffffffff ) {
 			 correctedStr = gp.string_resolve( strId ) ;
 		} else { // trying stemming
@@ -921,7 +918,7 @@ SpellCorrectResult QLexParser::trySpellCorrectAndClassify (PosedVec<CTWPVec> cPo
 			strId = bzSpell->getStemCorrection( stemmedStr, theString, lang, BZSpell::CORRECTION_MODE_NORMAL );
 			if( strId != 0xffffffff ) {
 				correctedStr = gp.string_resolve( strId ) ;
-			} else if( (stemmedStr.length() > MIN_SPELL_CORRECT_LEN) && strlen(theString) != stemmedStr.length() ) {
+			} else if( !m_dontSpell && (stemmedStr.length() > MIN_SPELL_CORRECT_LEN) && strlen(theString) != stemmedStr.length() ) {
                 // spelling correction failed but the string got stemmed
                 strId = bzSpell->getSpellCorrection( stemmedStr.c_str(), true, lang );
                 if( strId != 0xffffffff )
@@ -954,7 +951,7 @@ SpellCorrectResult QLexParser::trySpellCorrectAndClassify (PosedVec<CTWPVec> cPo
             SpellCorrectResult corrResult;
             QLexParser_LocalParms lparm(cPosVec, tPosVec, qparm, t_len, strId, lang,theString,bzSpell,ctok,ttok,t);
 
-            if( trySplitCorrect (corrResult, lparm, qparm.isAutoc ) ) 
+            if( !m_dontSpell && trySplitCorrect (corrResult, lparm, qparm.isAutoc ) ) 
                 return corrResult;
         }
         //// end of split correction attempt
