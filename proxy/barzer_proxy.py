d023b872b6752ff85478a7c63a8ab2418bae58ef
Max Eronin <reductor@gmail.com>
2013-02-07 22:01:42 +0400

    new key index and auth_keys format

--- proxy/barzer_proxy.py
+++ proxy/barzer_proxy.py
@@ -1,8 +1,9 @@
-import uwsgi #@UnresolvedImport
+import uwsgi, traceback, sys #@UnresolvedImport
 from uwsgi import cache_set, cache_get, cache_exists, cache_update #@UnresolvedImport
 from urlparse import unquote
 from xml.sax.saxutils import escape, quoteattr
 from os.path import exists
+from render import QState, reconcile, error_xml, error_json
 
 #copypasted from uwsgidecorators
 def get_free_signal():
@@ -13,28 +14,37 @@ def get_free_signal():
 
 BARZER_SOCKET = uwsgi.opt.get('barzer-socket') or "127.0.0.1:5767"
 KEY_FILE = uwsgi.opt.get('key_file')
+key_ix = {}
+
 def read_keys(num):
-    print "reading keys from", KEY_FILE
+    global key_ix
     if not exists(KEY_FILE):
         print KEY_FILE, "not found"
         return
     with open(KEY_FILE) as f:
-        uwsgi.cache_clear()
-        for l in f:
-            l = l.strip()
-            if not l: continue
-            k, v = l.split('|', 1)
-            if ',' in v:
-                vs = v.split(',')
-                cache_set(k, '^'+str(len(vs)))
-                for i, c in enumerate(vs):
-                    cache_set(k+str(i), c)
-            else:
-                cache_set(k, v)
+        print "reading keys from", KEY_FILE
+        try:
+            new_ix = {}
+            for l in f:
+                l = l.strip()
+                if not l: continue
+                k, v = l.split('|', 1)
+                lst = new_ix[k] = []
+                if v[0] == '|':
+                    for sub in v[1:].split('|'):
+                        i, s, a = sub.split(',', 2)
+                        lst.append((i, int(s), a))
+                else:
+                    for i in v.split(','):
+                        lst.append((i, 0, ''))
+            key_ix = new_ix
+        except Exception as e:
+            traceback.print_exc(file=sys.stderr)
+            print e
 
 def set_mon():
     num = get_free_signal()
-    uwsgi.register_signal(num, 'mule1', read_keys)
+    uwsgi.register_signal(num, 'workers', read_keys)
     uwsgi.add_file_monitor(num, KEY_FILE)
     read_keys(num)
 
@@ -42,24 +52,11 @@ if KEY_FILE is not None:
     set_mon()
 
 def resolve_key(key, uids):
-    val = cache_get(key)
-    if not val: return
-    elif val[0] == '^':
-        for i in xrange(int(val[1:])):
-            v = cache_get(key + str(i))
-            if v:
-                uids.append(v)
-            else:
-                print >>sys.stderr, "Error retrieving key {}{}".format(key, i)
-    else:
-        uids.append(val)
-    
+    for val in key_ix.get(key, []):
+        uids.append(val[0])
+
+
 ## socket operations
-class QState(object):
-    status = 0
-    result = ''
-    def __init__(self, uid):
-        self.uid = uid
 
 def register_all(fdmap): 
     """ Apparently you need to reregister all waits after every resume
@@ -71,36 +68,14 @@ def register_all(fdmap):
             uwsgi.wait_fd_write(fd, 10)
     return ''
 
-def close_all(fdmap):
-    for k in fdmap:
-        uwsgi.close(k)    
-
-## content
-
-def reconcile(responses, r_type):
-    def process_json():
-        return '[{}]'.format(',\n'.join(responses))
-    def process_xml():
-        return '<barz-list>{}</barz-list>'.format('\n'.join(responses))
-    return locals()['process_'+r_type]()
-
-def error_xml(s):
-    #return '<error>' + s + '</error>'
-    return "<error>{}</error>".format(s)
-def error_json(s):
-    return '{{ "error": "{}" }}'.format(s) 
-def error_html(s):
-    return '<h3 style="color:red">{}</h3>'.format(s)
 
 def application(env, start_response):
     typ = env['PATH_INFO'].rsplit('/', 1)[-1] #@ReservedAssignment
-    r_type = None 
     if typ in ('json', 'sjson'):
-        ct, error, add = 'application/json', error_json, ' ret="{}"'.format(typ)
-        r_type = 'json' 
+        add = ' ret="{}"'.format(typ)
+        ct, error, r_type = 'application/json', error_json, 'json'
     else:
-        ct, error, add = 'text/xml', error_xml, ''
-        r_type = 'xml'
+        ct, error, r_type, add = 'text/xml', error_xml, 'xml', ''
     start_response('200 OK', [('Content-Type', ct + '; charset=utf-8')])
 
     query = ''
@@ -142,36 +117,36 @@ def application(env, start_response):
         fd = uwsgi.async_connect('127.0.0.1:5767')
         fdmap[fd] = QState(i)
 
-    cnt = len(uids)
-    while cnt:
+    while fdmap:
         yield register_all(fdmap)
         fd = env['uwsgi.ready_fd']
-        if not uwsgi.is_connected(fd):
-            close_all(fdmap)
-            yield error("Unable to connect")
-            return        
-        elif env['x-wsgiorg.fdevent.timeout']:
-            close_all(fdmap)
-            yield error("Connection timed out")
-            return        
-
         st = fdmap[fd]
-        if st.status == 0:
+        if not uwsgi.is_connected(fd):
+            st.status = -1
+            st.result = error("Unable to connect") #fallthrough
+        elif env['x-wsgiorg.fdevent.timeout']:
+            st.status = -2
+            st.result = error("Connection timed out") #fallthrough
+        elif st.status == 0:
             qry = '<query u="{}"{}>{}</query>\r\n.\r\n'.format(st.uid, add, query)
             uwsgi.send(fd, qry)
             st.status = 1
+            continue
         elif st.status == 1:
             data = uwsgi.recv(fd)
             if data:
                 st.result += data
-            else:
-                responses.append(st.result)
-                del fdmap[fd]
-                uwsgi.close(fd)
-                cnt -= 1
+                continue
+            #fallthrough
+        
+        uwsgi.close(fd)        
+        del fdmap[fd]
+        responses.append(st)
+
+
     if len(responses) > 1:
         yield reconcile(responses, r_type)
     else:
-        yield responses[0]
+        yield responses[0].result
                 
 
