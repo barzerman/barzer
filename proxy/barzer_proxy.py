257f035c0f409a741612091011ea83d08e18a445
Max Eronin <reductor@gmail.com>
2013-02-07 17:30:50 +0400

    first iteration of reconciling proxy

--- proxy/barzer_proxy.py
+++ proxy/barzer_proxy.py
@@ -19,24 +19,70 @@ def read_keys(num):
         print KEY_FILE, "not found"
         return
     with open(KEY_FILE) as f:
+        uwsgi.cache_clear()
         for l in f:
             l = l.strip()
             if not l: continue
             k, v = l.split('|', 1)
-            if cache_exists(k):
-                cache_update(k, v)
+            if ',' in v:
+                vs = v.split(',')
+                cache_set(k, '^'+str(len(vs)))
+                for i, c in enumerate(vs):
+                    cache_set(k+str(i), c)
             else:
                 cache_set(k, v)
 
 def set_mon():
     num = get_free_signal()
     uwsgi.register_signal(num, 'mule1', read_keys)
-    uwsgi.add_file_monitor(num, KEY_FILE)    
+    uwsgi.add_file_monitor(num, KEY_FILE)
     read_keys(num)
 
 if KEY_FILE is not None:
     set_mon()
+
+def resolve_key(key, uids):
+    val = cache_get(key)
+    if not val: return
+    elif val[0] == '^':
+        for i in xrange(int(val[1:])):
+            v = cache_get(key + str(i))
+            if v:
+                uids.append(v)
+            else:
+                print >>sys.stderr, "Error retrieving key {}{}".format(key, i)
+    else:
+        uids.append(val)
     
+## socket operations
+class QState(object):
+    status = 0
+    result = ''
+    def __init__(self, uid):
+        self.uid = uid
+
+def register_all(fdmap): 
+    """ Apparently you need to reregister all waits after every resume
+        to be compatible with wsgi *shrug*"""
+    for fd, st in fdmap.iteritems():
+        if st.status:
+            uwsgi.wait_fd_read(fd, 10)
+        else:
+            uwsgi.wait_fd_write(fd, 10)
+    return ''
+
+def close_all(fdmap):
+    for k in fdmap:
+        uwsgi.close(k)    
+
+## content
+
+def reconcile(responses, r_type):
+    def process_json():
+        return '[{}]'.format(',\n'.join(responses))
+    def process_xml():
+        return '<barz-list>{}</barz-list>'.format('\n'.join(responses))
+    return locals()['process_'+r_type]()
 
 def error_xml(s):
     #return '<error>' + s + '</error>'
@@ -48,14 +94,17 @@ def error_html(s):
 
 def application(env, start_response):
     typ = env['PATH_INFO'].rsplit('/', 1)[-1] #@ReservedAssignment
+    r_type = None 
     if typ in ('json', 'sjson'):
         ct, error, add = 'application/json', error_json, ' ret="{}"'.format(typ)
+        r_type = 'json' 
     else:
         ct, error, add = 'text/xml', error_xml, ''
+        r_type = 'xml'
     start_response('200 OK', [('Content-Type', ct + '; charset=utf-8')])
 
     query = ''
-    uid = 0
+    uids = []
     ver = 1.0
     qs = env['QUERY_STRING']
     for kv in (s2 for s1 in qs.split('&') for s2 in s1.split(';')):
@@ -66,7 +115,7 @@ def application(env, start_response):
         if k == 'query':
             query = escape(unquote(v.replace('+', ' ')))
         elif k == 'key':
-            uid = cache_get(v)
+            resolve_key(v, uids)
         elif k == 'ver':
             try:
                 ver = float(v)
@@ -76,7 +125,7 @@ def application(env, start_response):
         elif k == 'now':
             add += ' now='+ quoteattr(unquote(v.replace('+', ' ')))
             
-    if not uid:
+    if not uids:
         yield error("Invalid key")
         return
     elif not query:
@@ -86,30 +135,43 @@ def application(env, start_response):
         yield error("Version not supported")
         return
 
-    fd = uwsgi.async_connect(BARZER_SOCKET)
-    yield uwsgi.wait_fd_write(fd, 5)
-    
-    if env['x-wsgiorg.fdevent.timeout']:
-        yield error("Connection timed out")
-        uwsgi.close(fd)
-        return
-    elif not uwsgi.is_connected(fd):
-        yield error("Unable to connect")
-        uwsgi.close(fd)
-        return
-    qry = '<query u="{}"{}>{}</query>\r\n.\r\n'.format(uid, add, query)
-    #print qry
-    uwsgi.send(fd, qry)
-    while True:
-        yield uwsgi.wait_fd_read(fd, 10)
-        if env['x-wsgiorg.fdevent.timeout']:
-            yield error("Connection timed out")
-            uwsgi.close(fd)
-            return
+    fdmap = {}
+    responses = []
 
-        data = uwsgi.recv(fd)
-        if not data:
-            break
-        yield data
-    uwsgi.close(fd)
+    for i in uids:
+        fd = uwsgi.async_connect('127.0.0.1:5767')
+        fdmap[fd] = QState(i)
+
+    cnt = len(uids)
+    while cnt:
+        yield register_all(fdmap)
+        fd = env['uwsgi.ready_fd']
+        if not uwsgi.is_connected(fd):
+            close_all(fdmap)
+            yield error("Unable to connect")
+            return        
+        elif env['x-wsgiorg.fdevent.timeout']:
+            close_all(fdmap)
+            yield error("Connection timed out")
+            return        
+
+        st = fdmap[fd]
+        if st.status == 0:
+            qry = '<query u="{}"{}>{}</query>\r\n.\r\n'.format(st.uid, add, query)
+            uwsgi.send(fd, qry)
+            st.status = 1
+        elif st.status == 1:
+            data = uwsgi.recv(fd)
+            if data:
+                st.result += data
+            else:
+                responses.append(st.result)
+                del fdmap[fd]
+                uwsgi.close(fd)
+                cnt -= 1
+    if len(responses) > 1:
+        yield reconcile(responses, r_type)
+    else:
+        yield responses[0]
+                
 
