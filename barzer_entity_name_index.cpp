c9e70d9be86333e66f08ef90f8ae32e0ab5b26ca
barzerman <andrey.yanis@gmail.com>
2013-03-19 13:38:07 -0400

    #509

--- barzer_entity_name_index.cpp
+++ barzer_entity_name_index.cpp
@@ -7,10 +7,17 @@ void BENI::addEntityClass( const StoredEntityClass& ec )
     const auto& theMap = d_universe.getDtaIdx().entPool.getEuidMap();
     BarzerEntity id( ec, 0 );
     size_t numNames =0;
+    std::vector<char> tmpBuf;
+    std::string dest;
     for( auto i = theMap.lower_bound( id );i!= theMap.end() && i->first.eclass == ec; ++i ) {
         const EntityData::EntProp* edata = d_universe.getEntPropData( i->first );
         if( edata && !edata->canonicName.empty() ) {
             d_storage.addWord( edata->canonicName.c_str(), i->first );
+
+            if( Lang::stringToLower( tmpBuf, dest, edata->canonicName ) ) 
+                d_storage.addWord( dest.c_str(), i->first );
+
+            
             ++numNames;
         }
     }
