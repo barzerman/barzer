f5c2e4887b7dc041fde2c06517e74694eacc5de2
barzerman <andrey.yanis@gmail.com>
2013-03-27 16:38:10 -0400

    #513

--- ay/ay_parse.h
+++ ay/ay_parse.h
@@ -29,13 +29,6 @@ void parse_separator( const CB&cb, const char* s, const char* s_end, char sep='|
     size_t tok_num = 0;
     for( ; b &&(b<s_end) && !cb( tok_num++, b, (pipe?pipe:s_end) ); b= (pipe?pipe+1:0), (pipe = (b? std::find(b,s_end,sep):0)) );
 }
-template <typename CB>
-void parse_separator( const CB&cb, const char* s, const char* s_end, char sep='|' )
-{
-    const char* b = s, *pipe = strchr(b,sep);
-    size_t tok_num = 0;
-    for( ; b &&(b<s_end) && !cb( tok_num++, b, (pipe?pipe:s_end) ); b= (pipe?pipe+1:0), (pipe = (b? std::find(b,s_end,sep):0)) );
-}
 
 struct separated_string_to_vec {
     std::vector< std::string >& theVec;
