cad476459efc03fc12c9f1a6e7fc5b68df1c3c61
barzerman <andrey.yanis@gmail.com>
2013-06-04 10:19:33 +0400

    #569 refactored translation out of trie.h

--- barzer_el_rewriter.h
+++ barzer_el_rewriter.h
@@ -7,6 +7,7 @@
 //#include <barzer_emitter.h>
 #include <barzer_el_parser.h>
 #include <barzer_el_chain.h>
+#include <barzer_el_eval.h>
 #include <ay/ay_vector.h>
 
 namespace barzer {
@@ -215,7 +216,8 @@ public:
         { return getLocalVar( varId ); }
 };
 
-struct BarzelEvalContext {
+class BarzelEvalContext {
+public:
 	BarzelMatchInfo& matchInfo;
 	const StoredUniverse& universe;
 	const BELTrie& d_trie;
@@ -308,64 +310,4 @@ private:
     {}
 };
 
-class BarzelEvalNode {
-public:
-    typedef std::vector< BarzelEvalNode > ChildVec;
-protected:
-	BTND_RewriteData d_btnd;
-
-	ChildVec d_child;
-	//// will recursively add nodes 
-public:
-	typedef std::pair< const uint8_t*, const uint8_t* > ByteRange;
-private:
-	bool isSubstitutionParm( size_t& pos ) const;
-	bool eval_comma(BarzelEvalResult&, BarzelEvalContext& ctxt ) const;
-public:
-    // returns true if this node if this is a LET node - let node assigns a variable 
-    const BTND_Rewrite_Control* getControl() const 
-        { return boost::get<BTND_Rewrite_Control>(&d_btnd); }
-    void setBtnd( const BTND_RewriteData& rd ) 
-        { d_btnd= rd; }
-	BTND_RewriteData& getBtnd() { return d_btnd; }
-	ChildVec& getChild() { return d_child; }
-
-    /// takes o's children and adds the ones that are new as children
-    /// children of o which already appear in d_child are not added.
-    /// returns the number of actual children added
-    size_t addNodesChildren(const BarzelEvalNode& o) ;
-
-    /// returns true if o's children are the same as this node's children  
-    bool hasSameChildren( const BarzelEvalNode& o) const;
-    
-    // adds this node as a child
-    BarzelEvalNode& addChild(const BarzelEvalNode& o) 
-    {
-        d_child.push_back(o);
-        return d_child.back();
-    }
-    BarzelEvalNode& addChild() 
-    {
-        d_child.resize( d_child.size()+1 );
-        return d_child.back();
-    }
-
-	const ChildVec& getChild() const { return d_child; }
-
-	const BTND_RewriteData& getBtnd() const { return d_btnd; }
-	bool isFallible() const ;
-	BarzelEvalNode() {}
-	BarzelEvalNode( const BTND_RewriteData& b ) : d_btnd(b) {}
-
-	/// returns true if evaluation is successful 
-	bool eval(BarzelEvalResult&, BarzelEvalContext& ctxt ) const;
-
-    const BTND_Rewrite_Control* isComma( ) const ;
-
-    bool equal( const BarzelEvalNode& other ) const;
-
-    const BarzelEvalNode* getSameChild( const BarzelEvalNode& other ) const;
-    BarzelEvalNode* getSameChild( const BarzelEvalNode& other ) ;
-};
-	
 } // barzer namespace 
--- barzer_el_trie.h
+++ barzer_el_trie.h
@@ -13,6 +13,7 @@
 #include <boost/thread/shared_mutex.hpp>
 #include <boost/unordered_map.hpp>
 #include <boost/thread/locks.hpp>
+#include <barzer_el_translation.h>
 
 
 /// data structures representing the Barzer Expression Language BarzEL term pattern trie
@@ -238,119 +239,8 @@ inline bool operator< (const BarzelWCLookupKey& l, const BarzelWCLookupKey& r )
 	);
 }
 
-/// right side of the pattern
-class BarzelTranslation {
-public:
-	typedef enum {
-		T_NONE,   // blank translation
-		T_STOP, // translates into stop token
-		T_STRING, // dynamic string or literal
-		T_COMPWORD, // compounded word
-		T_NUMBER_INT, // integer
-		T_NUMBER_REAL, // real number (float)
-        
-		/// this one is very special - this actually creates a tree
-		T_RAWTREE, // BELParseTreeNode as is (faster to compute)   
-
-		T_BLANK,    // blank literal
-		T_PUNCT,    // punctuation
-		T_VAR_NAME, // variable name rewrite
-		T_VAR_WC_NUM, // variable rewrite on wildcard number
-		T_VAR_EL_NUM, // variable rewrite on raw element number (PN)
-		T_VAR_GN_NUM, // variable rewrite on raw element number
-		T_MKENT, // entity maker
-		T_MKENTLIST, // entity list maker
-		T_FUNCTION, // fucntion(void)
-        T_REQUEST_VAR_NAME, // request wide variable MODE_REQUEST_VAR (RequestEnvironment::d_reqVar)
-
-		T_MAX
-	} Type_t;
-	boost::variant< uint32_t, double, int64_t> id;
-	BarzelTranslationTraceInfo traceInfo;
-	uint8_t  type; // one of T_XXX constants
-	uint8_t  makeUnmatchable; // when !=0 this is a terminating translation ... the value will
-                                // be assigned to the unmatchability of the affected beads
-
-    uint32_t argStrId;
-    int8_t  confidenceBoost; // can be between -CONFIDENCE_HIGH and +CONFIDENCE_HIGH 
-
-	void set( BELTrie& trie, const BTND_Rewrite_Literal& );
-	void set( BELTrie& trie, const BTND_Rewrite_Number& );
-	void set( BELTrie& trie, const BTND_Rewrite_MkEnt& );
-	void set( BELTrie& trie, const BTND_Rewrite_Variable& );
-	void set( BELTrie& trie, const BTND_Rewrite_Function& );
-	void setMkEntList( uint32_t i )
-	{
-		type = T_MKENTLIST;
-		id = i;
-	}
-
-	enum {
-		ENCOD_FAILED = -1,
-		ENCOD_RAWTREE=0,
-		ENCOD_TRIVIAL
-	};
-	int set( BELTrie& trie, const BELParseTreeNode& );
-
-	void setStop() { type = T_STOP; }
-
-	//// this is invoked from el_parser
-	template <typename T>
-	inline int setBtnd( BELTrie& trie, const T& x ) {
-		// AYDEBUG("unsupported trivial rewrite" );
-		setStop();
-        return 1;
-	}
-
-	void setRawTree( uint32_t rid )
-	{
-		type = T_RAWTREE;
-		id = rid;
-	}
-
-	uint8_t getType() const { return type; }
-	uint32_t getRewriterId() const
-        { return ( (type==T_RAWTREE) ? boost::get<uint32_t>(id) : 0xffffffff ); }
-
-	uint32_t getId_uint32() const { return ( id.which() ==0 ? boost::get<uint32_t>(id) : 0xffffffff ); }
-	int64_t getId_int() const { return ( id.which() ==2  ? boost::get<int64_t>(id) : 0 ); }
-	double getId_double() const { return ( id.which() ==1  ? boost::get<double>(id) : 0.0 ); }
-    uint32_t getArgStrId() const { return argStrId; }
-
-
-	void clear() { type= T_NONE; }
-	bool isMkEntSingle() const  { return ( type == T_MKENT ); }
-	bool isMkEntList() const  { return ( type == T_MKENTLIST ); }
-    bool isEntity() const { return ( isMkEntSingle() || isMkEntList() ); }
-	bool isRawTree() const  { return ( type == T_RAWTREE ); }
-
-	/// resolves
-	void fillRewriteData( BTND_RewriteData& ) const;
-
-	bool nonEmpty() const
-		{ return ( type != T_NONE ); }
-	BarzelTranslation() : id( 0xffffffff ) , type(T_NONE), makeUnmatchable(0), argStrId(0xffffffff), confidenceBoost(0) {}
-	// BarzelTranslation(Type_t t , uint32_t i ) : id(i),type((uint8_t)t), makeUnmatchable(0), argStrId(0xffffffff) {}
-
-	std::ostream& print( std::ostream& , const BELPrintContext& ) const;
-
-};
-template <>
-inline int BarzelTranslation::setBtnd<BTND_Rewrite_Function>( BELTrie& trie, const BTND_Rewrite_Function& x ) {
-	set(trie,x);
-    return 0;
-}
-template <>
-inline int BarzelTranslation::setBtnd<BTND_Rewrite_Literal>( BELTrie& trie, const BTND_Rewrite_Literal& x ) { set(trie,x); return 0;}
-template <>
-inline int BarzelTranslation::setBtnd<BTND_Rewrite_Number>( BELTrie& trie, const BTND_Rewrite_Number& x ) { set(trie,x); return 0; }
-template <>
-inline int BarzelTranslation::setBtnd<BTND_Rewrite_Variable>( BELTrie& trie, const BTND_Rewrite_Variable& x ) { set(trie,x); return 0; }
-template <>
-inline int BarzelTranslation::setBtnd<BTND_Rewrite_MkEnt>( BELTrie& trie, const BTND_Rewrite_MkEnt& x ) { set(trie,x); return 0; }
 class BarzelTrieNode;
 typedef std::map<BarzelTrieFirmChildKey, BarzelTrieNode > BarzelFCMap;
-/// BarzelFirmChildPool contiguously stores BarzelFCMap objects (see el_trie.h for definition)
 typedef ay::PoolWithId< BarzelFCMap > BarzelFirmChildPool;
 typedef ay::PoolWithId< BarzelTranslation > BarzelTranslationPool;
 
