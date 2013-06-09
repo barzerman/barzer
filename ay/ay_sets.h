c59d298c747ea41436a8a083e98f63cc7e5f34a8
0xd34df00d <0xd34df00d@gmail.com>
2013-06-09 13:03:54 +0400

    Take skips into account for findXSections().

--- ay/ay_sets.h
+++ ay/ay_sets.h
@@ -12,10 +12,18 @@
 namespace ay
 {
 	template<typename T>
-	using RangesVec = std::vector<std::pair<T, T>>;
+	struct Range
+	{
+		T first;
+		T second;
+		size_t skip;
+	};
+	
+	template<typename T>
+	using RangesVec = std::vector<Range<T>>;
 
 	template<typename T>
-	RangesVec<T> findXSections(T hayStart, T hayEnd, T neeStart, T neeEnd, size_t minLength = 0)
+	RangesVec<T> findXSections(T hayStart, T hayEnd, T neeStart, T neeEnd, size_t minLength = 0, size_t skipLength = 0)
 	{
 		if (neeStart == neeEnd || hayStart == hayEnd)
 			return {};
@@ -42,14 +50,33 @@ namespace ay
 		RangesVec<T> result;
 		for (size_t c = 0; c < cols - minLength; ++c)
 		{
+			size_t currentSkip = 0;
+			size_t totalSkip = 0;
 			for (size_t r = 0; r < rows - minLength; ++r)
 			{
 				if (!matrix[c][r])
 					continue;
 
 				size_t nc = c, nr = r;
-				while (nc < cols && nr < rows && matrix[nc][nr])
+				while (nc < cols && nr < rows)
 				{
+					if (!matrix[nc][nr])
+					{
+						if (currentSkip >= skipLength)
+						{
+							nc -= currentSkip;
+							nr -= currentSkip;
+							break;
+						}
+						else
+							++currentSkip;
+					}
+					else
+					{
+						totalSkip += currentSkip;
+						currentSkip = 0;
+					}
+					
 					matrix[nc][nr] = false;
 					++nc;
 					++nr;
@@ -61,9 +88,10 @@ namespace ay
 				auto rStart = neeStart, rEnd = neeStart;
 				std::advance(rStart, c);
 				std::advance(rEnd, nc);
-				result.push_back({ rStart, rEnd });
+				result.push_back({ rStart, rEnd, totalSkip });
 			}
 		}
+		
 		std::sort(result.begin(), result.end(),
 				[](const typename RangesVec<T>::value_type& l, const typename RangesVec<T>::value_type& r)
 					{ return std::distance(l.first, l.second) > std::distance(r.first, r.second); });
