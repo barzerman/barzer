015b01dcfe716e2d7bf8d011240846fcbd077811
0xd34df00d <0xd34df00d@gmail.com>
2013-09-08 01:33:11 +0400

    Consistent stringnum #614.

--- bson/stringnum.h
+++ bson/stringnum.h
@@ -1,38 +1,68 @@
-#pragma 
+#pragma once
 
 /// implements an incrementable string representation of 20 digit decimals (max int 64 is 18,446,744,073,709,551,615)
 /// the intended use is this: you start with an index and increment it by 1
 /// 32bit unsigned is practically enough 
-#include <stdint.h>
-#include <stdio.h>
-namespace ebson11 {
 
-class StrRepDecimal {
-    char d_buf[ 21 ];
-    char* d_first;
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+
+namespace ebson11
+{
+class StrRepDecimal
+{
+	char d_buf[21];
+	char *d_first;
 public:
-    StrRepDecimal() : 
-        d_first(d_buf+sizeof(d_buf)-2)
-        { 
-            *d_first= '0';
-            d_first[1]=0;
-        }
+	StrRepDecimal()
+	: d_first(d_buf + sizeof(d_buf) - 2)
+	{
+		*d_first = '0';
+		d_first[1] = 0;
+	}
 
-    void slowInit( uint32_t i ) { snprintf( d_buf, sizeof(d_buf), "%u", i ); }
-    inline void increment() {
-        for( char* p = (d_buf+sizeof(d_buf)-2); p >=d_first; --p ) {
-            if( *p>= '9' )
-                *p= '0';
-            else {
-                ++(*p);
-                return;
-            }
-        }
-        if( d_first > d_buf )
-            *(--d_first) = '1';
-    }
+	explicit StrRepDecimal(uint32_t i)
+	{
+		*this = i;
+	}
 
-    const char* c_str() const {  return d_first; }
+	StrRepDecimal(const StrRepDecimal& other)
+	{
+		std::memcpy(d_buf, other.d_buf, sizeof(d_buf));
+		d_first = d_buf + (other.d_first - other.d_buf);
+	}
+
+	StrRepDecimal& operator=(uint32_t i)
+	{
+		std::snprintf(d_buf, sizeof(d_buf), "%u", i);
+		return *this;
+	}
+
+    inline void increment()
+	{
+		for(char* p = d_buf + sizeof(d_buf) - 2; p >= d_first; --p)
+		{
+			if (*p >= '9')
+				*p = '0';
+			else
+			{
+				++(*p);
+				return;
+			}
+		}
+
+		if(d_first > d_buf)
+			*(--d_first) = '1';
+	}
+
+	const char* operator++()
+	{
+		increment();
+		return c_str();
+	}
+
+	const char* c_str() const { return d_first; }
 };
 
 }
