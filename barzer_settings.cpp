7c6c6ab01af71627ef345f45d0b79c476d3e2f92
barzerman <andrey.yanis@gmail.com>
2013-09-27 16:44:24 -0400

    testing rebuild

--- barzer_settings.cpp
+++ barzer_settings.cpp
@@ -1034,5 +1034,4 @@ const std::string BarzerSettings::get(const char *key) const {
 const std::string BarzerSettings::get(const std::string &key) const {
 	return get(key.c_str());
 }
-#error SHIT
 }
