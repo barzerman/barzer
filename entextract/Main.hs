df5a92200bb963c0e6e9c0ac688438a3107b2925
0xd34df00d <0xd34df00d@gmail.com>
2013-10-23 22:03:22 +0400

    Working with articles heuristic for #638.

--- entextract/Main.hs
+++ entextract/Main.hs
@@ -1,12 +1,22 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 
+import System.Environment
 import Control.Arrow
+import Control.Monad
 import qualified Data.Map.Strict as M
 import qualified Data.Text as T
 import Data.List
 import NLP.Snowball
 
 gramCount = 3
+markers = map T.pack ["a", "the"]
+popThreshold = 2
+
+data LinkedText = LinkedText {
+        stemmed :: T.Text,
+        origText :: T.Text
+    }
+mkLinked t = LinkedText (stem English t) t
 
 genGrams c l | length l < c = []
              | otherwise = take c l : genGrams c (tail l)
@@ -17,16 +27,30 @@ parse2map = M.fromListWith (++) . map (second (fixText . drop 1) . break (== '|'
 grammize = M.map $ genGramsU gramCount
 
 invert = M.foldrWithKey (\d a m -> foldr (foldrer d) m a) M.empty
-foldrer :: String -> [T.Text] -> M.Map [T.Text] [String] -> M.Map [T.Text] [String]
-foldrer d i = M.insertWith (++) i [d]
+    where foldrer d i = M.insertWith (++) i [d]
 
 toStats = M.map length
 
---reduceN c m = M.filterWithKey (\k v -> 
+-- Remove sub-ngrams if they have the same statistical count.
+reduce m = foldr foldrer m $ M.keys m
+    where foldrer k m' = let l = M.lookup k m' in foldr (M.alter (f' l)) m' $ genGramsU (length k) k \\ [k]
+          f' (Just v) jc@(Just c) | v == c = Nothing
+                                  | otherwise = jc
+          f' _ _ = Nothing
 
-process cs = undefined
+articlesHeur = M.filterWithKey (\g c -> c <= popThreshold && head g `elem` markers)
 
-main' f = do
-    c <- readFile f
-    let d = process c
-    return ()
+process = articlesHeur . prepare
+
+prepare = reduce . toStats . invert . grammize . parse2map
+
+format = unlines . map T.unpack . map (T.intercalate $ T.singleton ' ') . map (map (\t -> if t `elem` markers then (T.pack "opt(") `T.append` t `T.append` (T.singleton ')') else t)) . M.keys
+
+main' f = readFile f >>= (putStrLn . format . process)
+
+main = do
+    pname <- getProgName
+    args <- getArgs
+    if length args == 0
+        then print $ "Usage: " ++ pname ++ " datafile.txt"
+        else main' $ head args
