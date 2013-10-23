394c352db357c54213ad91eeec07f701ad84d28e
0xd34df00d <0xd34df00d@gmail.com>
2013-10-23 23:37:52 +0400

    Filter shit & keep stems #638.

--- entextract/Main.hs
+++ entextract/Main.hs
@@ -6,23 +6,33 @@ import Control.Monad
 import qualified Data.Map.Strict as M
 import qualified Data.Text as T
 import Data.List
+import Data.Ord
 import NLP.Snowball
 
 gramCount = 3
 markers = map T.pack ["a", "the"]
+endShit = map T.pack ["a", "the", "in", "on", "to", "from", "of", "for", "until", "and", "or", "but", "of", "if", "i", "we", "us", "you"]
+bothShit = map T.pack ["and", "or", "but", "of", "if", "i", "we", "us", "you"]
 popThreshold = 2
 
 data LinkedText = LinkedText {
         stemmed :: T.Text,
         origText :: T.Text
-    }
+    } deriving (Show)
+instance Ord LinkedText where
+    compare = comparing stemmed
+instance Eq LinkedText where
+    x == y = stemmed x == stemmed y
+
 mkLinked t = LinkedText (stem English t) t
+
+type RevMap = M.Map [LinkedText] Int
 
 genGrams c l | length l < c = []
              | otherwise = take c l : genGrams c (tail l)
 genGramsU c l = concatMap (`genGrams` l) [1..c]
 
-fixText = stems English . map (T.toLower . T.pack) . words . map (\c -> if c `elem` "?”“" then ' ' else c) . filter (`notElem` "-'")
+fixText = map (mkLinked . T.toLower . T.pack) . words . map (\c -> if c `elem` "?”“" then ' ' else c) . filter (`notElem` "-'")
 parse2map = M.fromListWith (++) . map (second (fixText . drop 1) . break (== '|')) . lines
 grammize = M.map $ genGramsU gramCount
 
@@ -38,19 +48,26 @@ reduce m = foldr foldrer m $ M.keys m
                                   | otherwise = jc
           f' _ _ = Nothing
 
-articlesHeur = M.filterWithKey (\g c -> c <= popThreshold && head g `elem` markers)
+filterShit :: RevMap -> RevMap
+filterShit = M.filterWithKey (\g c -> stemmed (head g) `notElem` bothShit && stemmed (last g) `notElem` endShit)
 
+articlesHeur :: RevMap -> RevMap
+articlesHeur = M.filterWithKey (\g c -> c <= popThreshold && stemmed (head g) `elem` markers)
+
+process :: String -> RevMap
 process = articlesHeur . prepare
 
-prepare = reduce . toStats . invert . grammize . parse2map
+prepare :: String -> RevMap 
+prepare = filterShit . reduce . toStats . invert . grammize . parse2map
 
-format = unlines . map T.unpack . map (T.intercalate $ T.singleton ' ') . map (map (\t -> if t `elem` markers then (T.pack "opt(") `T.append` t `T.append` (T.singleton ')') else t)) . M.keys
+format :: RevMap -> String
+format = unlines . map T.unpack . map (T.intercalate $ T.singleton ' ') . map (map (\t' -> let t = origText t' in if t `elem` markers then T.pack "opt(" `T.append` t `T.append` T.singleton ')' else t)) . M.keys
 
 main' f = readFile f >>= (putStrLn . format . process)
 
 main = do
     pname <- getProgName
     args <- getArgs
-    if length args == 0
+    if null args
         then print $ "Usage: " ++ pname ++ " datafile.txt"
         else main' $ head args
