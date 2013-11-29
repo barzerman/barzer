7450386fe96b7a51da2ea6eebbf26666880d64c2
barzerman <andrey.yanis@gmail.com>
2013-11-29 16:16:18 -0500

    #651 tagindex tested

--- ay/ay_tagindex.h
+++ ay/ay_tagindex.h
@@ -16,12 +16,16 @@ template <typename I>
 class tagindex {
     typedef std::set<I> IdSet;
     typedef std::map<std::string,IdSet> TagMap;
-
     TagMap d_theMap;
     struct SetIter_comp_less { bool operator()( typename TagMap::const_iterator l, typename TagMap::const_iterator r ) const { return ( l->first < r->first ); } };
     typedef std::set<typename TagMap::const_iterator,SetIter_comp_less> TagMapIterVec;
     std::map< I, TagMapIterVec> d_id2TagIter;
 public:
+    const IdSet* getTagIdSetPtr( const char* t ) const
+    {
+        auto x = d_theMap.find(t);
+        return( x == d_theMap.end() ? 0 : &(x->second) );
+    }
 
     bool hasTag( const I& id, const char* tag )  const
     {
@@ -83,7 +87,7 @@ class tagindex_checker {
     const tagindex<I>* d_idx;
     
     tagindex_checker( const tagindex_checker& ) {}
-    typedef const typename tagindex<I>::IdSet IdSetPtr;
+    typedef const typename tagindex<I>::IdSet* IdSetPtr;
     std::vector<IdSetPtr> d_setVec;
 public: 
     tagindex_checker( ) : d_idx(0) {}
@@ -97,9 +101,9 @@ public:
     bool addTag( const char* t ) 
     {
         if( !d_idx ) return false;
-        auto x = d_idx->find( t );
-        if( x != d_idx->end() ) 
-            d_setVec.push_back( &(x->second) );
+        auto x = d_idx->getTagIdSetPtr( t );
+        if( x )
+            d_setVec.push_back( x );
         return true;
     }
     bool hasAnyTags( const I& id ) 
@@ -126,7 +130,26 @@ public:
     size_t visitAllIds( const CB& cb )
     {
         if( empty() ) return 0;
-        #warning traverse sets one by one  
+        auto shortVecI = d_setVec.begin();
+        /// looking up the shortest set 
+        for( auto i = shortVecI+1; i!= d_setVec.end(); ++i ) 
+            if( (*i)->size() > (*shortVecI)->size() )
+                shortVecI = i;
+        size_t count = 0;
+        for( auto i = (*shortVecI)->begin(); i != (*shortVecI)->end(); ++i ) {
+            bool goodForCallback = true;
+            for( auto otherVec = d_setVec.begin(); otherVec!= d_setVec.end(); ++otherVec ) {
+                if( otherVec != shortVecI && (*otherVec)->find( *i ) == (*otherVec)->end() ) {
+                    goodForCallback= false;
+                    break;
+                }
+            }
+            if( goodForCallback ) {
+                d_idx->visitAllTagsOfId( [&]( const std::string& tag ) { cb( *i, tag );}, *i );
+                ++count;
+            }
+        }
+        return count;
     }
 };
 
